00744c2bc09e207b5b7f9df32713b46eec72d544
FrontendSuberu <joeysuberu@gmail.com>
2025-10-14 02:05:08 +0100

    scrolltrigger created + gsap initialized

--- src/main.js
+++ src/main.js
@@ -1,3 +1,22 @@
-document.addEventListener("DOMContentLoaded", function() {
-  
-})
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+document.addEventListener("DOMContentLoaded", function () {
+  const libWrapper = document.querySelector(".library");
+  let libText = document.querySelectorAll(".content");
+
+
+
+  // scrolltrigger to pin the element to the viewport
+  ScrollTrigger.create({
+    trigger: libWrapper,
+    start: "top top",
+    end: "bottom 200px",
+    pin: true,
+    // pinSpacing: false,
+    markers: true
+  })
+
+});
