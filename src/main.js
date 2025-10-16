570230f264ce3eec7f97fb25cd3c51cbbae503d0
FrontendSuberu <joeysuberu@gmail.com>
2025-10-16 13:29:12 +0100

    tested out using event listener on scrollTrigger and it did not give end result

--- src/main.js
+++ src/main.js
@@ -5,26 +5,29 @@ document.addEventListener("DOMContentLoaded", function () {
   gsap.registerPlugin(ScrollTrigger);
 
   let libText = gsap.utils.toArray(".content");
-  let libTexts = document.querySelectorAll(".content");
+  const container = document.querySelector(".library")
   const total = libText.length;
 
-  gsap.from(libText, {
+
+
+  gsap.set(libText, {
     yPercent: 400,
     opacity: 0,
   });
 
   ScrollTrigger.create({
-    trigger: libText[0],
-    start: "120px 500px",
-    end: () => "+=" + libText.length * 150,
-    toggleActions: "play none play reverse",
+    trigger: container,
+    start: "top top",
+    end: "bottom bottom",
+    // end: () => "+=" + libText.length * 150,
+    // toggleActions: "play none play reverse",
     markers: true,
     onUpdate: (self) => {
       if (self.isActive) {
         const p = self.progress; // 0 → 1 for entire scroll range
         const slice = 1 / total; // portion of progress for each element
 
-        libTexts.forEach((el, i) => {
+        libText.forEach((el, i) => {
           let local = (p - i * slice) / slice;
           local = Math.min(Math.max(local, 0), 1); // clamp between 0–1
 
