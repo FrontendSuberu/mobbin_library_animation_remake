c2ece0af44ef12d64b61df8b928bdac472a6237e
FrontendSuberu <joeysuberu@gmail.com>
2025-10-16 02:26:34 +0100

    pushing major update 01

--- src/main.js
+++ src/main.js
@@ -1,22 +1,35 @@
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-gsap.registerPlugin(ScrollTrigger);
-
 document.addEventListener("DOMContentLoaded", function () {
-  const libWrapper = document.querySelector(".library");
-  let libText = document.querySelectorAll(".content");
+  gsap.registerPlugin(ScrollTrigger);
 
+  let libText = gsap.utils.toArray(".content");
+  const total = libText.length;
 
+  gsap.set(libText, { yPercent: 400 });
 
-  // scrolltrigger to pin the element to the viewport
   ScrollTrigger.create({
-    trigger: libWrapper,
-    start: "top top",
-    end: "bottom 200px",
-    pin: true,
-    // pinSpacing: false,
-    markers: true
-  })
+    trigger: libText[0],
+    start: "top 500px",
+    endTrigger: libText[libText.length - 1],
+    end: () => "+=" + libText.length * 150,
+    markers: true,
+    onUpdate: (self) => {
+      if (self.isActive) {
+        const p = self.progress; // 0 → 1 for entire scroll range
+        const slice = 1 / total;
 
+        sections.forEach((el, i) => {
+          // map global progress into each element’s slice (0–1)
+          let local = (p - i * slice) / slice;
+          local = Math.min(Math.max(local, 0), 1); // clamp between 0–1
+
+          gsap.set(el, {
+            yPercent: 400 * (1 - local),
+          });
+        });
+      }
+    },
+  });
 });
